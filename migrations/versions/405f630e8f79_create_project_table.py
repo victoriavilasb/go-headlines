596c276bfb0b880aad41b4019cb754de6e2eee3f
victoriaovilas <victoriaovilas+github@gmail.com>
2022-10-16 00:08:59 -0300

    feat: add database with project table as test and example

---
+++ migrations/versions/405f630e8f79_create_project_table.py
@@ -0,0 +1,34 @@
+"""create project table
+
+Revision ID: 405f630e8f79
+Revises: 
+Create Date: 2022-10-15 23:50:13.706025
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from enum import Enum
+
+
+# revision identifiers, used by Alembic.
+revision = '405f630e8f79'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+class ProjectStatus(Enum):
+    archived = 'ARCHIVED'
+    active = 'ACTIVE'
+
+
+def upgrade() -> None:
+    op.create_table(
+        'project',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('name', sa.String()),
+        sa.Column('status', sa.Enum(ProjectStatus), default=ProjectStatus.active)
+    )
+
+
+def downgrade() -> None:
+    op.drop_table('project')
