9a42d8acc71435eef4df145fb4063fb2e1f119db
victoriaovilas <victoriaovilas+github@gmail.com>
2022-10-26 14:01:53 -0300

    add some tests

--- tests/app/test_command.py
+++ tests/app/test_command.py
@@ -2,19 +2,36 @@ import unittest
 
 from unittest.mock import MagicMock
 from ttrack.repository.database.command import Command
-from ttrack.repository.database.models import Project
+from ttrack.repository.database.models import Project, ProjectStatus, Task
 
 class TestCommand(unittest.TestCase):
 
+    def setUp(self):
+        self.session = MagicMock()
+        self.command = Command(self.session)
+
     def test_create_project(self):
-        session = MagicMock()
-        command = Command(session)
-
-        command.create_project('fake-project-name')
+        self.command.create_project('fake-project-name')
         
-        session.add.assert_called_once()
-        session.commit.assert_called_once()
+        self.session.add.assert_called_once()
+        self.session.commit.assert_called_once()
 
+    def test_archive_project(self):
+        self.command.archive_project('fake-project-name')
+
+        self.session.update.return_value.where.return_value.values.assert_called_with(status = ProjectStatus.archived)
+        self.session.commit.assert_called_once()
+
+    def test_create_task(self):
+        project = Project(name='fake-project-name')
+
+        self.command.create_task('fake-task-name')
+
+        self.session.query.return_value.filter.return_value.one.return_value(project)
+        self.session.add.assert_called_once()
+        self.session.commit.assert_called_once()
+
+        
 
 
 
--- ttrack/repository/database/command.py
+++ ttrack/repository/database/command.py
@@ -1,6 +1,6 @@
 # Here we will have methods that modify the database 
 
-from ttrack.repository.database.models import Project, Task
+from ttrack.repository.database.models import Project, ProjectStatus, Task
 
 class Command:
     def __init__(self, session):
@@ -12,16 +12,22 @@ class Command:
         self.session.commit()
 
     def create_task(self, name, project_name = None):
-        project = self._get_project_id_by_name(project_name) or {}
-        t = Task(name=name, project_id=project.id)
+        project = self._get_project_by_name(project_name)
+        project_id = None
+
+        if project != None:
+            project_id = project.id
+            
+        t = Task(name=name, project_id=project_id)
         self.session.add(t)
         self.session.commit()
 
     def archive_project(self, name):
-        pass
+        self.session.update(Project).where(Project.name == name).values(status = ProjectStatus.archived)
+        self.session.commit()
 
-    def list_projects():
-        pass
+    def list_projects(self):
+        self.session.query(Project).all()
 
     def _get_project_by_name(self, project_name=None):
         if project_name != None:
