ec1ca5529e46292a58b2b0a2cf7539d5e4133718
victoriaovilas <victoriaovilas+github@gmail.com>
2022-11-05 06:52:13 -0300

    refactor: extract methods from start task method

--- ttrack/app/task.py
+++ ttrack/app/task.py
@@ -15,19 +15,9 @@ class TaskApplication:
         self.project_name = project_name
 
     def start(self, force: bool):
-        if self._check_if_given_project_does_not_exist():
-            return
+        self._check_if_given_project_does_not_exist()
             
-        running_task = self._get_running_task()
-        if running_task:
-            if force:
-                self.storage.update_task_status(
-                    running_task["name"],
-                    self.Status.paused.value
-                )
-            else:
-                print("ERROR: {} is running. You should finish or pause this task before starting another.".format(running_task["name"]))
-                return
+        self. _pause_running_tasks_if_forced(force)
         
         self.storage.create_task(self.name, self.project_name)
 
@@ -114,7 +104,18 @@ class TaskApplication:
         if self.project_name and len(self.storage.find_project(self.project_name)) == 0:
             print("ERROR: this project does not exist. You should choose an existent project.")
             print("run ttrack list --project to see all projects available")
-            return True
-        return False
+            quit()
         
+    def _pause_running_tasks_if_forced(self, force: bool):
+        running_task = self._get_running_task()
+        if len(running_task) == 0:
+            return
+        elif force:
+            self.storage.update_task_status(
+                    running_task["name"],
+                    self.Status.paused.value
+                )
+        else:
+            print("ERROR: {} is running. You should finish or pause this task before starting another.".format(running_task["name"]))
+            quit()
 
