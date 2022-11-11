785d15585faa3e2d8cb5173580bd13ee10d920a2
victoriaovilas <victoriaovilas+github@gmail.com>
2022-11-11 14:14:34 -0300

    fix yaml wrapper

--- ttrack/repository/yaml.py
+++ ttrack/repository/yaml.py
@@ -2,16 +2,16 @@ import os
 import yaml
 
 class Yaml:
-    def read_config_file(file_path):
+    def read_file(self, file_path):
         with open(file_path) as f:
             return yaml.safe_load(f)
 
     def write_file(self, file_path, data):
-        if not self.file_exists():
+        if not self.file_exists(file_path):
             os.makedirs(os.path.dirname(file_path))
             
-        with open(self.file_path, 'w') as yaml_file:
+        with open(file_path, 'w') as yaml_file:
             yaml.dump(data, yaml_file, default_flow_style=False)
 
-    def file_exists(file_path):
+    def file_exists(self, file_path):
         return os.path.exists(os.path.dirname(file_path))
