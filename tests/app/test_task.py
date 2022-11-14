7c8e723ef14ddf4637dec99487b4f177dfa5054b
victoriaovilas <victoriaovilas+github@gmail.com>
2022-11-14 06:56:56 -0300

    add fixture

--- tests/app/test_task.py
+++ tests/app/test_task.py
@@ -5,43 +5,40 @@ from ttrack.repository.storage import Storage
 from ttrack.app.task import TaskApplication
 
 class TestTask(unittest.TestCase):
+    
+    def setUp(self):
+        self.storage = Storage()
+        self.task_name = 'task-name'
 
     @patch.object(Storage, 'find_project', return_value={'name': 'project-name'})
     @patch.object(Storage, 'list_tasks', return_value=[])
     @patch.object(Storage, 'create_task')
     def test_start_when_project_is_given(self, create_task, list_tasks, find_project):
-        storage = Storage()
         project = 'project-name'
-        task_name = 'task-name'
 
-        task = TaskApplication(storage, task_name, project)
+        task = TaskApplication(self.storage, self.task_name, project)
 
         task.start(force=False)
 
         find_project.assert_called_with(project)
         list_tasks.assert_called_with(status='RUNNING')
-        create_task.assert_called_with(task_name, project)
+        create_task.assert_called_with(self.task_name, project)
 
     @patch.object(Storage, 'list_tasks', return_value=[])
     @patch.object(Storage, 'create_task')
     def test_start_when_project_is_not_given(self, create_task, list_tasks):
-        storage = Storage()
-        task_name = 'task-name'
-
-        task = TaskApplication(storage, task_name, None)
+        task = TaskApplication(self.storage, self.task_name, None)
 
         task.start(force=False)
 
         list_tasks.assert_called_with(status='RUNNING')
-        create_task.assert_called_with(task_name, None)
+        create_task.assert_called_with(self.task_name, None)
 
     @patch.object(Storage, 'find_project', return_value={})
     def test_start_when_project_is_given_and_does_not_exists(self, find_project):
-        storage = Storage()
         project = 'project-name'
-        task_name = 'task-name'
 
-        task = TaskApplication(storage, task_name, project)
+        task = TaskApplication(self.storage, self.task_name, project)
 
         with self.assertRaises(SystemExit):
             task.start(force=False)
@@ -50,10 +47,7 @@ class TestTask(unittest.TestCase):
 
     @patch.object(Storage, 'list_tasks', return_value=[{'name': 'running_task', 'status': 'RUNNING'}])
     def test_start_when_a_running_task_exists_and_force_false(self, list_tasks):
-        storage = Storage()
-        task_name = 'task-name'
-
-        task = TaskApplication(storage, task_name, None)
+        task = TaskApplication(self.storage, self.task_name, None)
         
         with self.assertRaises(SystemExit):
             task.start(force=False)
@@ -64,13 +58,12 @@ class TestTask(unittest.TestCase):
     @patch.object(Storage, 'create_task')
     @patch.object(Storage, 'update_task_status')
     def test_start_when_a_running_task_exists_and_force_true(self, update_task_status, create_task, list_tasks):
-        storage = Storage()
-        task_name = 'task-name'
-
-        task = TaskApplication(storage, task_name, None)
+        task = TaskApplication(self.storage, self.task_name, None)
         
         task.start(force=True)
 
         list_tasks.assert_called_with(status='RUNNING')
         update_task_status.assert_called_with('running-task', 'PAUSED')
-        create_task.assert_called_with(task_name, None)
+        create_task.assert_called_with(self.task_name, None)
+
+
